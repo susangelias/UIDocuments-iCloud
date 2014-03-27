d1c4f4e172919dbf4c9665649df1cc67727ac977
Susan Elias <susangelias@gmail.com>
2014-03-27 14:42:12 -0400

    Added explicit target action set up for the Edit/Done button so that it is not dependent on the storyboard where I’m swapping these classes into another project

--- GuidesLocal/DocumentViewController.m
+++ GuidesLocal/DocumentViewController.m
@@ -46,6 +46,10 @@ NSString * const editDoneButtonTitleDone = @"Done";
     
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    
+    // Make sure Edit/Done button has the correct target action
+    self.editButtonItem.target = self;
+    self.editButtonItem.action = @selector(EditDoneButtonPressed:);
 }
 
 
